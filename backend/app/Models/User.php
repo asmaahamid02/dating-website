edfa4249e8f90964eb0332a673967e1b806a9083
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-01 22:30:01 +0300

    backend: added the fillable fields

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'gender',
+        'interested_in',
+        'country',
+        'city'
     ];
 
     /**
