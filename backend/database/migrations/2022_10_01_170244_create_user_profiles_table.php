6261f9f6cba29c1bfc5633d6a1d83a0250124047
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-01 20:07:27 +0300

    backend: created the project then added users tables migrations

---
+++ backend/database/migrations/2022_10_01_170244_create_user_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('nickname');
+            $table->string('bio')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
