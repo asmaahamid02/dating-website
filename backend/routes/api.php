48274b48efeb24864eca9cc66db9ea834d9344b3
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-01 22:32:02 +0300

    backend: added the Auth routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'v0.1'], function () {
+    Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    });
 });
