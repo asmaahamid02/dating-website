b1d89f27c1582d616bb2e58dc15455ad12faccd0
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-02 15:53:40 +0300

    backend: changed the sanctum middleware to api middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,9 +19,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix' => 'v0.1'], function () {
-    Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
-    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+        Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+        Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
         Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 
         ####Start Users Group####
