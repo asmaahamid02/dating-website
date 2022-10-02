8f45b1c0d8a94a9b973e7a1b136af08254c98ea9
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-02 13:44:50 +0300

    backend: added relations to BlockedUser model

--- backend/app/Models/BlockedUser.php
+++ backend/app/Models/BlockedUser.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class BlockedUser extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'blocked_user_id'];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function favoriteUsers()
+    {
+        return $this->belongsTo(User::class, 'blocked_user_id');
+    }
 }
