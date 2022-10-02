04bd0b29b4743f0da9ace1642047b75c99e46aed
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-02 19:05:21 +0300

    backend: added messages routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,20 +2,10 @@
 
 use App\Http\Controllers\BlockedUserController;
 use App\Http\Controllers\FavoriteUserController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 
 Route::group(['prefix' => 'v0.1'], function () {
@@ -45,5 +35,12 @@ Route::group(['prefix' => 'v0.1'], function () {
             Route::get('/{id}', [BlockedUserController::class, 'updateBlockStatus']);
         });
         ####End Users Block####
+
+        ####Start Messages####
+        Route::group(['prefix' => 'messages'], function () {
+            Route::get('/{sender_id}/{receiver_id}', [MessageController::class, 'getMessages']);
+            Route::get('/send/{receiver_id}', [MessageController::class, 'sendMessage']);
+        });
+        ####End Messages####
     });
 });
