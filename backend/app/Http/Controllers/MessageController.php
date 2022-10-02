0570eae3c308cd9435afb11b5419d08aca5dff9c
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-02 16:13:40 +0300

    backend: created sendMessage function

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
+use App\Models\User;
 use App\Traits\ResponseJson;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class MessageController extends Controller
@@ -25,5 +27,33 @@ class MessageController extends Controller
             return $this->jsonResponse($messages, 'data', Response::HTTP_OK);
 
         return $this->jsonResponse(null, 'data', Response::HTTP_NOT_FOUND, 'No Messages');
+    }
+
+    public function sendMessage(Request $request, $receiver_id)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'message' => 'required|min:1'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->jsonResponse($validator->errors(), 'data', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $sender_id = Auth::id();
+        $message = $request->message;
+        $recipient = User::where('id', $receiver_id)->where('is_visible', 1)->first();
+
+        if ($recipient) {
+            Message::create([
+                'sender_id', $sender_id,
+                'receiver_id', $receiver_id,
+                'message', $message
+            ]);
+
+            return $this->jsonResponse($message, 'data', Response::HTTP_OK);
+        }
+
+        return $this->jsonResponse(null, 'data', Response::HTTP_NOT_FOUND, 'User not found');
     }
 }
