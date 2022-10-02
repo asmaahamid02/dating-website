e762d951011591a1bb95dc8c35cc5ad801fb3e65
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-02 13:50:34 +0300

    backend: added index and update functions to BlockedUserController

--- backend/app/Http/Controllers/BlockedUserController.php
+++ backend/app/Http/Controllers/BlockedUserController.php
@@ -2,83 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BlockedUser;
+use App\Models\User;
+use App\Traits\ResponseJson;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlockedUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    use ResponseJson;
     public function index()
     {
-        //
+        $blocked_users = BlockedUser::where('user_id', Auth::id())
+            ->with('BlockedUsers')
+            ->get();
+
+        if ($blocked_users->isNotEmpty())
+            return $this->jsonResponse($blocked_users, 'data', Response::HTTP_OK);
+
+        return $this->jsonResponse('No Blocked Users', 'message', Response::HTTP_NOT_FOUND);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function update($blocked_user_id)
     {
-        //
-    }
+        //get the user who will added to blocked list
+        $user = User::where('id', $blocked_user_id)->where('id', '!=', Auth::id())->first();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        if ($user) {
+            //check if the user is in the block list
+            $blocked = BlockedUser::where('user_id', Auth::id())
+                ->where('blocked_user_id', $blocked_user_id)
+                ->first();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+            //if found delete it
+            if ($blocked)
+                //delete
+                $blocked->delete();
+            //else create it    
+            else
+                //create
+                BlockedUser::create([
+                    'user_id' => Auth::id(),
+                    'blocked_user_id' => $blocked_user_id
+                ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+            return $this->jsonResponse('Updated Successfully', 'message', Response::HTTP_OK);
+        }
+        return $this->jsonResponse('User not found', 'message', Response::HTTP_NOT_FOUND);
     }
 }
