c68244dc1a63a115ca5b9700fbdb6e0bba5e851b
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-02 11:48:35 +0300

    backend: added route for show user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,6 +24,7 @@ Route::group(['prefix' => 'v0.1'], function () {
 
         Route::group(['prefix' => 'users'], function () {
             Route::get('/', [UserController::class, 'index']);
+            Route::get('/{id}', [UserController::class, 'show']);
             Route::patch('/{id}', [UserController::class, 'update']);
         });
     });
