e173694ba669589e667c2e3c63d99e5658bfcbd3
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-03 17:10:16 +0300

    frontend: added  signup function and refactored the code

--- frontend/scripts/auth.js
+++ frontend/scripts/auth.js
@@ -1,52 +1,63 @@
 const login_form = document.getElementById('login')
 const signup_form = document.getElementById('signup')
-
-const name_input = document.querySelector('#name')
-const name_error_message = document.querySelector('#input-name')
-
-const email = document.getElementById('email')
-const email_error_message = document.getElementById('input-email')
-
 const password = document.getElementById('password')
-const password_error_message = document.getElementById('input-password')
-
-const password_conf = document.querySelector('#password-confirm')
-const password_conf_error_message = document.querySelector(
-  '#input-password-confirm'
-)
-
-const gender = document.getElementById('gender')
-const gender_error_message = document.getElementById('input-gender')
-
-const interested_in = document.getElementById('interested_in')
-const interested_in_error_message = document.getElementById('input-interested')
-
-const country = document.getElementById('country')
-const country_error_message = document.getElementById('input-country')
-
-const city = document.getElementById('city')
-const city_error_message = document.getElementById('input-city')
-
 const success_message = document.querySelector('.success-message')
 const error_message = document.querySelector('.error-message')
+const inputs = Array.from(
+  document.querySelectorAll('.input-container > :first-child')
+)
+const error_messages = Array.from(document.querySelectorAll('.input-message'))
 
 //stop the page from auto-reload
 window.stop()
 
+//function used to check if the forms fields are valid
+const validateInputs = () => {
+  let isValid = true
+  for (let i = 0; i < inputs.length; i++) {
+    if (
+      !validations.isNotEmpty(inputs[i], error_messages[i], inputs[i].name) ||
+      (inputs[i].name == 'password' &&
+        !validations.strongPassword(
+          inputs[i],
+          error_messages[i],
+          inputs[i].name
+        )) ||
+      (inputs[i].name == 'email' &&
+        !validations.validEmail(inputs[i], error_messages[i])) ||
+      (inputs[i].name == 'password-confirm' &&
+        !validations.checkPasswordMatch(password, inputs[i], error_messages[i]))
+    ) {
+      isValid = false
+      break
+    }
+  }
+  return isValid
+}
+
+//function to display success message
+const displaySuccessMessage = (message) => {
+  //display success message
+  success_message.innerText = message
+  error_message.classList.add('hide')
+  success_message.classList.remove('hide')
+}
+
+//function to display error message
+const displayErrorMessage = (message) => {
+  //display error message
+  error_message.textContent = message
+  success_message.classList.add('hide')
+  error_message.classList.remove('hide')
+}
+
+//login funcction
 const login = () => {
   login_form.addEventListener('submit', async (e) => {
     e.preventDefault()
     //validate inputs
-    if (
-      validations.isNotEmpty(email, email_error_message, 'Email') &&
-      validations.validEmail(email, email_error_message) &&
-      validations.isNotEmpty(password, password_error_message, 'password') &&
-      validations.validLength(password, 5, password_error_message, 'password')
-    ) {
-      const credentials = {
-        email: email.value,
-        password: password.value,
-      }
+    if (validateInputs()) {
+      const credentials = new FormData(login_form)
 
       //send request to login api
       const response = await common.postAPI(
@@ -67,76 +78,34 @@ const login = () => {
         localStorage.setItem('__DateUser', JSON.stringify(userData))
 
         //display success message
-        success_message.innerText = response.message
-        error_message.classList.add('hide')
-        success_message.classList.remove('hide')
+        displaySuccessMessage(response.message)
 
         //move to the homepage
         common.changeRoute('homepage.html')
       } else {
         //error
-        if (response.data) {
-          error_message.innerText = response.data
-        } else {
-          error_message.innerText = response.message
-        }
-        success_message.classList.add('hide')
-        error_message.classList.remove('hide')
+        displayErrorMessage(response.data ? response.data : response.message)
       }
     } else {
       //prevented
-      success_message.classList.add('hide')
-      error_message.classList.remove('hide')
-      error_message.textContent = 'Fields are not valid'
+      displayErrorMessage('Fields are not valid')
       e.preventDefault()
     }
   })
 }
 
-const inputs = Array.from(
-  document.querySelectorAll('.input-container > :first-child')
-)
-
-const error_messages = Array.from(document.querySelectorAll('.input-message'))
-
+//signup function
 const signup = () => {
   signup_form.addEventListener('submit', async (e) => {
     e.preventDefault()
     //validate inputs
-    let flag = true
-    for (let i = 0; i < inputs.length; i++) {
-      if (
-        !validations.isNotEmpty(inputs[i], error_messages[i], inputs[i].name) ||
-        (inputs[i].name == 'password' &&
-          !validations.strongPassword(
-            inputs[i],
-            error_messages[i],
-            inputs[i].name
-          )) ||
-        (inputs[i].name == 'email' &&
-          !validations.validEmail(inputs[i], error_messages[i])) ||
-        (inputs[i].name == 'password-confirm' &&
-          !validations.checkPasswordMatch(
-            password,
-            inputs[i],
-            error_messages[i]
-          ))
-      ) {
-        flag = false
-        break
-      }
-    }
 
-    if (!flag) {
-      return
+    if (!validateInputs()) {
+      //prevented
+      displayErrorMessage('Fields are not valid')
+      e.preventDefault()
     } else {
-      let userData = {}
-      for (let j = 0; j < inputs.length; j++) {
-        if (inputs[j].name == 'password-confirm') continue
-
-        userData[inputs[j].name] = inputs[j].value
-      }
-
+      let userData = new FormData(signup_form)
       const response = await common.postAPI(
         `${common.baseURL}/register`,
         userData
@@ -144,23 +113,17 @@ const signup = () => {
 
       if (response.statusCode < 400) {
         //success
+        displaySuccessMessage(response.message)
 
-        //display success message
-        success_message.innerText = response.message
-        error_message.classList.add('hide')
-        success_message.classList.remove('hide')
-
-        //move to the homepage
+        //move to the login page
         common.changeRoute('index.html')
       } else {
         //error
-        if (response.data && response.data.email) {
-          error_message.innerText = response.data.email
-        } else {
-          error_message.innerText = response.message
-        }
-        success_message.classList.add('hide')
-        error_message.classList.remove('hide')
+        displayErrorMessage(
+          response.data && response.data.email
+            ? response.data.email
+            : response.message
+        )
       }
     }
   })
