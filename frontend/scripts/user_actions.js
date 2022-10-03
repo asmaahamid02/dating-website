163dfb07094e9769515460f6d71f8090794062ea
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-03 23:29:12 +0300

    frontend: created get messages function

--- frontend/scripts/user_actions.js
+++ frontend/scripts/user_actions.js
@@ -19,3 +19,28 @@ const blockUser = (id) => async () => {
 
   common.refresh()
 }
+
+const chat_container = document.querySelector('.chat-container')
+const getMessages = (receiver_id) => async () => {
+  const response = await common.getAPI(
+    `${common.baseURL}/${common.userID}/${receiver_id}`,
+    common.token
+  )
+  response.data.forEach((user) => {
+    if (user.id == common.userID) {
+      user.sender.forEach((message) => {
+        chat_container.appendChild(
+          createMessages('receiver', message.pivot.message)
+        )
+      })
+    } else {
+      user.sender.forEach((message) => {
+        chat_container.appendChild(
+          createMessages('sender', message.pivot.message)
+        )
+      })
+    }
+  })
+}
+
+const sendMessage = receiver_id
