9d849e8ce0bee397271de16cd26854536eb23d2e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-03 21:10:14 +0300

    frontend: added favorite and block actions

--- frontend/scripts/user_actions.js
+++ frontend/scripts/user_actions.js
@@ -1,1 +1,21 @@
-const favoriteUser = (id) => () => {}
+const favoriteUser = (id) => async () => {
+  const response = await common.getAPI(
+    `${common.baseURL}/favorites/${id}`,
+    common.token
+  )
+
+  common.Console('Favorit', [response.message])
+
+  common.refresh()
+}
+
+const blockUser = (id) => async () => {
+  const response = await common.getAPI(
+    `${common.baseURL}/blocks/${id}`,
+    common.token
+  )
+
+  common.Console('Blocked', [response.message])
+
+  common.refresh()
+}
