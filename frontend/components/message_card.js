5d2a7c54beecf91e6e938969179b67056fb44e1e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-03 23:27:39 +0300

    frontend: created message card component

---
+++ frontend/components/message_card.js
@@ -0,0 +1,25 @@
+const createUsers = (user) => {
+  //user container
+  const newUser = document.createElement('div')
+  newUser.classList.add('user')
+  newUser.id = `${user.id}`
+
+  let element = document.createElement('h3')
+  element.innerText = `${user.name}`
+
+  element.addEventListener('click', getMessages(user.id))
+  newUser.appendChild(element)
+
+  element = document.createElement('hr')
+  newUser.appendChild(element)
+
+  return newUser
+}
+
+const createMessages = (className, text) => {
+  const message = document.createElement('p')
+  message.classList.add(className)
+  message.innerText = text
+
+  return message
+}
