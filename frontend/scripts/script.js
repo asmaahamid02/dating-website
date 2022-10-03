592b7bf692c11339e61254184f8c6efc42c65434
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-03 18:01:40 +0300

    frontend: passed the token to axios repsonses

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -12,18 +12,26 @@ common.Console = (title, values) => {
   console.log('--/' + title + '---')
 }
 
-common.getAPI = async (api_url) => {
+common.getAPI = async (api_url, token = null) => {
   try {
-    const data = await axios.get(api_url)
+    const data = await axios.get(api_url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
     return data.data
   } catch (error) {
     common.Console('Error from GET API', [error])
   }
 }
 
-common.postAPI = async (api_url, api_data) => {
+common.postAPI = async (api_url, api_data, token = null) => {
   try {
-    const response = await axios.post(api_url, api_data)
+    const response = await axios.post(api_url, api_data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
     return response.data
   } catch (error) {
     // common.Console('Error from POST API', [error])
@@ -43,8 +51,12 @@ common.changeRoute = (page) => {
   }, 1000)
 }
 
-common.userID = localStorage.getItem('user')
-  ? JSON.parse(localStorage.getItem('user')).id
+common.userID = localStorage.getItem('__DateUser')
+  ? JSON.parse(localStorage.getItem('__DateUser')).id
+  : null
+
+common.token = localStorage.getItem('__DateUser')
+  ? JSON.parse(localStorage.getItem('__DateUser')).token
   : null
 
 common.pathName = window.location.pathname
