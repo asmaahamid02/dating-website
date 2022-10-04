7acfa0a13fcce784647379fcf6b0ddc4107093ba
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-04 13:33:37 +0300

    frontend: changed favorite button based on action

--- frontend/components/profile_card.js
+++ frontend/components/profile_card.js
@@ -2,6 +2,37 @@ const default_image = './assets/images/png/user-blank.png'
 const profiles_path = '../backend/storage/app/public/users/profiles/'
 const createProfileCard = (user) => {
   //card container
+  let is_favorited = false
+
+  //check if the logged user favorites $this user through relationship
+  if (user.favoritor) {
+    for (let row of user.favoritor) {
+      if (row.id == common.userID) {
+        is_favorited = true
+        break
+      }
+    }
+  }
+
+  //check if the logged user favorites $this user through pivot table
+  if (
+    user.favorites &&
+    user.favorites.user_id &&
+    user.favorites.user_id == common.userID
+  ) {
+    is_favorited = true
+  }
+
+  let is_blocked = false
+
+  if (
+    user.blocks &&
+    user.blocks.user_id &&
+    user.blocks.user_id == common.userID
+  ) {
+    is_blocked = true
+  }
+
   const newCard = document.createElement('div')
   newCard.classList.add('card')
   newCard.id = `${user.id}`
@@ -32,6 +63,12 @@ const createProfileCard = (user) => {
   element.classList.add('bio')
   newCard.appendChild(element)
 
+  //location section
+  element = document.createElement('p')
+  element.innerText = `${user.country} - ${user.city}`
+  element.classList.add('bio')
+  newCard.appendChild(element)
+
   element = document.createElement('div')
   element.classList.add('actions')
 
@@ -41,12 +78,16 @@ const createProfileCard = (user) => {
   newElement.classList.add('btn-fav')
 
   let child = document.createElement('img')
-  child.src = './assets/images/svg/favorite-svgrepo-com.svg'
+  if (is_favorited) {
+    child.src = './assets/images/svg/filled-heart-svgrepo-com.svg'
+  } else {
+    child.src = './assets/images/svg/heart-svgrepo-com.svg'
+  }
 
   newElement.appendChild(child)
 
   //favorite action
-  newElement.addEventListener('click', favoriteUser(`${user.id}`))
+  newElement.addEventListener('click', favoriteUser(`${user.id}`, child))
 
   element.appendChild(newElement)
 
@@ -56,7 +97,12 @@ const createProfileCard = (user) => {
   newElement.classList.add('btn-block')
 
   child = document.createElement('img')
-  child.src = './assets/images/svg/block-svgrepo-com.svg'
+
+  if (is_blocked) {
+    child.src = './assets/images/svg/unblock-svgrepo-com.svg'
+  } else {
+    child.src = './assets/images/svg/block-svgrepo-com.svg'
+  }
 
   newElement.appendChild(child)
 
