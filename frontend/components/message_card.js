6006e33f1438a5979368ab1774a31bd8a127bf39
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-04 21:34:25 +0300

    frontend: modified createUsers function, added functions for sender and receiver messages

--- frontend/components/message_card.js
+++ frontend/components/message_card.js
@@ -1,17 +1,29 @@
-const createUsers = (user, chatContainer) => {
+const default_image = './assets/images/png/user-blank.png'
+const profiles_path = '../backend/storage/app/public/users/profiles/'
+
+const createUsers = (user, containers) => {
   //user container
   const newUser = document.createElement('div')
   newUser.classList.add('user')
+  newUser.classList.add('flex')
   newUser.id = `${user.id}`
 
-  let element = document.createElement('h3')
+  let element = document.createElement('img')
+  element.classList.add('round-image')
+  element.classList.add('mr')
+  element.src = user.profile
+    ? `${profiles_path}${user.profile.profile_picture}`
+    : `${default_image}`
+
+  newUser.appendChild(element)
+
+  element = document.createElement('h3')
+  element.classList.add('username')
   element.innerText = `${user.name}`
 
-  element.addEventListener('click', getMessages(user.id, chatContainer))
   newUser.appendChild(element)
 
-  element = document.createElement('hr')
-  newUser.appendChild(element)
+  newUser.addEventListener('click', () => getMessages(user, containers))
 
   return newUser
 }
@@ -22,4 +34,44 @@ const createMessages = (className, text) => {
   message.innerText = text
 
   return message
+}
+
+const createSenderDiv = (text, imageName = null) => {
+  const mainDiv = document.createElement('div')
+  mainDiv.classList.add('sender')
+  mainDiv.classList.add('flex')
+
+  let element = document.createElement('p')
+  element.innerText = text
+
+  mainDiv.appendChild(element)
+
+  element = document.createElement('img')
+  element.classList.add('round-image')
+  element.classList.add('ml')
+  element.src = imageName ? `${profiles_path}${imageName}` : `${default_image}`
+
+  mainDiv.appendChild(element)
+
+  return mainDiv
+}
+
+const createReceiverDiv = (text, imageName = null) => {
+  const mainDiv = document.createElement('div')
+  mainDiv.classList.add('receiver')
+  mainDiv.classList.add('flex')
+
+  let element = document.createElement('img')
+  element.classList.add('round-image')
+  element.classList.add('mr')
+  element.src = imageName ? `${profiles_path}${imageName}` : `${default_image}`
+
+  mainDiv.appendChild(element)
+
+  element = document.createElement('p')
+  element.innerText = text
+
+  mainDiv.appendChild(element)
+
+  return mainDiv
 }
